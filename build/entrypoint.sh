7484b9caab14f22329cc08503675991dd3d5df24
Aljoscha Dembowsky <aljoscha.dembowsky@otto.de>
2026-03-06 10:58:47 +0100

    refactor(entrypoint): remove VARNISH_VSM env variable
    To change the path to the VSM just append the parameter "-n" parameter to the command.

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-VARNISH_VSM="${VARNISH_VSM:-/var/lib/varnish}"
-
 if [ "$#" -eq 0 ]; then
-  set -- prometheus-varnish-exporter -n "${VARNISH_VSM}"
+  set -- prometheus-varnish-exporter
 elif [[ "$1" == -* ]]; then
   # If the first argument starts with a dash, prepend the default command
   set -- prometheus-varnish-exporter "$@"
+else
+  # Otherwise, use the provided command as is
+  set -- "$@"
 fi
 
 exec "$@"
